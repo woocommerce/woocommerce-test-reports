f7ca85eb1e55cd453c3000e8166a8f419bf67028
rodel <rodel.calasagsag@automattic.com>
2022-07-13 16:30:31 +0800

    Add logging on successful download

--- .github/workflows/scripts/download-report.sh
+++ .github/workflows/scripts/download-report.sh
@@ -6,7 +6,7 @@ WAIT_TIME=10
 
 for ((i = 1; i <= $MAX_ATTEMPTS; i++)); do
 
-    echo "Downloading $ARTIFACT_NAME (try #$i)..."
+    echo "Downloading $ARTIFACT_NAME to $DOWNLOAD_PATH (try #$i)..."
     gh run download $RUN_ID \
         --name $ARTIFACT_NAME \
         --dir $DOWNLOAD_PATH \
@@ -15,6 +15,8 @@ for ((i = 1; i <= $MAX_ATTEMPTS; i++)); do
     EXIT_CODE=$(echo $?)
 
     if [[ $EXIT_CODE -eq 0 ]]; then
+        echo "Success! The following files were downloaded to $DOWNLOAD_PATH:"
+        tree $DOWNLOAD_PATH
         break
     fi
 
