319baf4f2a9c513daf82b19d2c507cbe70ead620
Rodel <rodel.calasagsag@automattic.com>
2023-01-25 12:01:23 +0800

    Verify exit code too

--- .github/workflows/scripts/merge-changes-to-trunk.sh
+++ .github/workflows/scripts/merge-changes-to-trunk.sh
@@ -14,10 +14,11 @@ git checkout -b $BRANCH_NAME
 # Commit changes
 git add .
 commit_result=$(git commit -m "$BOT_COMMIT_MESSAGE" -m "Workflow run: https://github.com/woocommerce/woocommerce/actions/runs/$RUN_ID")
+commit_exit_code=$(exit $?)
 echo $commit_result
 
 # Gracefully exit when there's nothing to commit
-if [[ $commit_result == *"nothing to commit"* ]]
+if [[ $commit_result == *"nothing to commit"* && $commit_exit_code != 0 ]]
     then exit 0;
 fi
 
