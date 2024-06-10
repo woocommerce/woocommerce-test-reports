23f6f84bb1ca4830cf7d39eada86a94fffdd4073
Jon Lane <jon.lane@automattic.com>
2024-06-10 15:28:16 -0700

    Don’t write data if execution time is 0

--- .github/workflows/scripts/update-e2e-stats.js
+++ .github/workflows/scripts/update-e2e-stats.js
@@ -1,167 +1,173 @@
-const { GoogleAuth } = require('google-auth-library');
-const { google } = require('googleapis');
+const { GoogleAuth } = require("google-auth-library");
+const { google } = require("googleapis");
 const data = require(process.env.E2E_SUMMARY_JSON);
-const credentials = require('./key.json');
+const credentials = require("./key.json");
 
 // Extract the required information
 const { total, skipped, broken, failed } = data.statistic;
-const duration = ( data.time.duration ?? 0 ) / 1000;
+const duration = (data.time.duration ?? 0) / 1000;
 const dateObj = new Date();
-const formattedDate = dateObj.toLocaleDateString('en-GB', {
-  day: 'numeric',
-  month: 'short',
-  year: 'numeric'
+const formattedDate = dateObj.toLocaleDateString("en-GB", {
+  day: "numeric",
+  month: "short",
+  year: "numeric",
 });
-const [day, month, year] = formattedDate.split(' ');
+const [day, month, year] = formattedDate.split(" ");
 const shortMonth = month.substring(0, 3);
 const currentDate = `${day}-${shortMonth}-${year}`;
 // Google Sheet ID and range
-const spreadsheetId = '1UjaaOCSGNLWW5hYdtw52P5s_PTBWexHW4_CmwHwvCps';
+const spreadsheetId = "1UjaaOCSGNLWW5hYdtw52P5s_PTBWexHW4_CmwHwvCps";
 
 // Credentials and authentication
 // Create an instance of GoogleAuth from the credentials
 const auth = new GoogleAuth({
   credentials: credentials,
-  scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
 // Size of test suite
 async function appendSuiteSize() {
-  const range = 'Size of Test Suite!A:B'; // Specify the range where you want to write the data
+  const range = "Size of Test Suite!A:B"; // Specify the range where you want to write the data
   try {
     // Obtain the client
     const client = await auth.getClient();
 
     // Create a new instance of the Sheets API
-    const sheets = google.sheets({ version: 'v4', auth });
+    const sheets = google.sheets({ version: "v4", auth });
 
     // Prepare the request body
     const request = {
       spreadsheetId: spreadsheetId,
       range: range,
-      valueInputOption: 'USER_ENTERED',
+      valueInputOption: "USER_ENTERED",
       resource: {
-        values: [[currentDate, total]]
-      }
+        values: [[currentDate, total]],
+      },
     };
 
     // Send the request to append values to the sheet
     const response = await sheets.spreadsheets.values.append(request);
     console.log(`${response.data.updates.updatedCells} cells appended.`);
   } catch (err) {
-    console.error('Error appending to Google Sheet:', err);
+    console.error("Error appending to Google Sheet:", err);
   }
 }
 
 // Execution time
 async function appendExecutionTime() {
-  const range = 'Execution Time!A:B'; // Specify the range where you want to write the data
+  const range = "Execution Time!A:B"; // Specify the range where you want to write the data
   try {
     // Obtain the client
     const client = await auth.getClient();
 
     // Create a new instance of the Sheets API
-    const sheets = google.sheets({ version: 'v4', auth });
+    const sheets = google.sheets({ version: "v4", auth });
+
+    // Check if duration is 0
+    if (duration === 0) {
+      console.log("Duration is 0. Skipping the execution.");
+      return;
+    }
 
     // Prepare the request body
     const request = {
       spreadsheetId: spreadsheetId,
       range: range,
-      valueInputOption: 'USER_ENTERED',
+      valueInputOption: "USER_ENTERED",
       resource: {
-        values: [[currentDate, duration]]
-      }
+        values: [[currentDate, duration]],
+      },
     };
 
     // Send the request to append values to the sheet
     const response = await sheets.spreadsheets.values.append(request);
     console.log(`${response.data.updates.updatedCells} cells appended.`);
   } catch (err) {
-    console.error('Error appending to Google Sheet:', err);
+    console.error("Error appending to Google Sheet:", err);
   }
 }
 
 // Failed Tests
 async function appendFailures() {
-  const range = 'Number of Failures!A:B'; // Specify the range where you want to write the data
+  const range = "Number of Failures!A:B"; // Specify the range where you want to write the data
   try {
     // Obtain the client
     const client = await auth.getClient();
 
     // Create a new instance of the Sheets API
-    const sheets = google.sheets({ version: 'v4', auth });
+    const sheets = google.sheets({ version: "v4", auth });
 
     // Prepare the request body
     const request = {
       spreadsheetId: spreadsheetId,
       range: range,
-      valueInputOption: 'USER_ENTERED',
+      valueInputOption: "USER_ENTERED",
       resource: {
-        values: [[currentDate, failed]]
-      }
+        values: [[currentDate, failed]],
+      },
     };
 
     // Send the request to append values to the sheet
     const response = await sheets.spreadsheets.values.append(request);
     console.log(`${response.data.updates.updatedCells} cells appended.`);
   } catch (err) {
-    console.error('Error appending to Google Sheet:', err);
+    console.error("Error appending to Google Sheet:", err);
   }
 }
 
 // Skipped Tests
 async function appendSkipped() {
-  const range = 'Number of Skipped!A:B'; // Specify the range where you want to write the data
+  const range = "Number of Skipped!A:B"; // Specify the range where you want to write the data
   try {
     // Obtain the client
     const client = await auth.getClient();
 
     // Create a new instance of the Sheets API
-    const sheets = google.sheets({ version: 'v4', auth });
+    const sheets = google.sheets({ version: "v4", auth });
 
     // Prepare the request body
     const request = {
       spreadsheetId: spreadsheetId,
       range: range,
-      valueInputOption: 'USER_ENTERED',
+      valueInputOption: "USER_ENTERED",
       resource: {
-        values: [[currentDate, skipped]]
-      }
+        values: [[currentDate, skipped]],
+      },
     };
 
     // Send the request to append values to the sheet
     const response = await sheets.spreadsheets.values.append(request);
     console.log(`${response.data.updates.updatedCells} cells appended.`);
   } catch (err) {
-    console.error('Error appending to Google Sheet:', err);
+    console.error("Error appending to Google Sheet:", err);
   }
 }
 
 // Skipped Tests
 async function appendBroken() {
-  const range = 'Number of Broken!A:B'; // Specify the range where you want to write the data
+  const range = "Number of Broken!A:B"; // Specify the range where you want to write the data
   try {
     // Obtain the client
     const client = await auth.getClient();
 
     // Create a new instance of the Sheets API
-    const sheets = google.sheets({ version: 'v4', auth });
+    const sheets = google.sheets({ version: "v4", auth });
 
     // Prepare the request body
     const request = {
       spreadsheetId: spreadsheetId,
       range: range,
-      valueInputOption: 'USER_ENTERED',
+      valueInputOption: "USER_ENTERED",
       resource: {
-        values: [[currentDate, broken]]
-      }
+        values: [[currentDate, broken]],
+      },
     };
 
     // Send the request to append values to the sheet
     const response = await sheets.spreadsheets.values.append(request);
     console.log(`${response.data.updates.updatedCells} cells appended.`);
   } catch (err) {
-    console.error('Error appending to Google Sheet:', err);
+    console.error("Error appending to Google Sheet:", err);
   }
 }
 
@@ -172,7 +178,7 @@ if (today.getDate() === 1) {
   // Call the append function
   appendSuiteSize();
 } else {
-  console.log('Not the first day of the month. Skipping the execution.');
+  console.log("Not the first day of the month. Skipping the execution.");
 }
 
 // Everything else runs daily
