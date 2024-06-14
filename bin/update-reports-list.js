a64fdf8915b8cf53472a58cc3172d88ea28971d1
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-06-14 14:21:15 +0300

    Group reports by event and group name

--- bin/update-reports-list.js
+++ bin/update-reports-list.js
@@ -4,86 +4,99 @@
  * It will read data from $reportID/report/widgets/summary.json, $reportID/metadata.json
  */
 
-const { readS3Object, readJson } = require( './utils' );
+const { readS3Object, readJson, writeJson} = require( './utils' );
 const path = require( 'path' );
 const { PutObjectCommand } = require( '@aws-sdk/client-s3' );
 const { s3Params, s3client } = require( './s3-client' );
 
 
-const reportPath = process.env.REPORT_PATH;
+const localReportPath = process.env.REPORT_PATH;
 
-if ( ! reportPath ) {
+if ( ! localReportPath ) {
 	throw 'REPORT_PATH env variable is not set';
 }
 
-let json = { reports: [] };
-
 ( async () => {
 	// Get the existing reports list
-	const reportsData = ( await readS3Object( 'data/reports.json' ) ).toString() || '{"reports":[]}';
-	json = JSON.parse( reportsData );
-	await updateReportData(reportPath);
+	const reportsData = ( await readS3Object( 'data/reports.json' ) ).toString() || '{}';
+	const json = JSON.parse( reportsData );
+	const updatedJson = await updateReportData(localReportPath, json);
 
 	// Write the updated reports list locally
-	// writeJson( json, path.join( "", 'reports.json' ) );
+	writeJson( updatedJson, path.join( "", 'reports.json' ) );
 
 	// Upload the report to S3
 	const cmd = new PutObjectCommand( {
 		Bucket: s3Params.Bucket,
 		Key: 'data/reports.json',
-		Body: JSON.stringify( json, null, 2 ),
+		Body: JSON.stringify( updatedJson, null, 2 ),
 		ContentType: 'application/json',
 	} );
-	await s3client.send( cmd );
+	// await s3client.send( cmd );
 } )();
 
-async function updateReportData( reportPath ) {
-	const reportId = process.env.REPORT_ID
+async function updateReportData( reportPath, json ) {
+	// Get the metadata
+	const metadata = readJson( path.join( reportPath, 'metadata.json' ) )
+	const  { report_id, event_name, group } = metadata;
 
-	if ( ! reportId ) {
-	throw 'REPORT_ID env variable is not set';
-}
+	if ( ! report_id ) {
+		throw 'Cannot find report_id in metadata.json';
+	}
+
+	// Get the event node or create it if it doesn't exist
+	if ( ! json[event_name] ) {
+		json[event_name] = {};
+	}
+
+	// Get the group node or create it if it doesn't exist
+	if ( ! json[event_name][group] ) {
+		// Add the properties that should belong to the group -
+		// all the reports in this group should have the same values
+		json[event_name][group] = {
+			lastUpdate: new Date().toISOString(),
+			pr_number: metadata.pr_number,
+			report_title: metadata.report_title,
+			ref_name: metadata.ref_name,
+			sha: metadata.sha,
+			reports: [],
+		};
+	}
 
 	// Get the report statistics from report/widgets/summary.json
 	const statistic = readJson( path.join( reportPath, 'widgets/summary.json' ) ).statistic;
-
-	// Get the metadata
-	const metadata = readJson( path.join( reportPath, 'metadata.json' ) ) || {
-		"suite": "",
-        "ref_name": "",
-        "run_id": "",
-        "event_name": "",
-        "report_title": "",
-        "pr_number": "",
-        "sha": "",
-        "updated_on": ""
+	const { total, passed, failed, skipped, broken, unknown } = statistic;
+	const results = {
+		passed,
+		failed: failed + broken + unknown,
+		skipped,
+		total,
 	};
 
 	// Create the report entry
 	const isFailed = statistic.total !== statistic.passed + statistic.skipped;
 	const report = {
-		id: reportId,
-		lastUpdate: metadata.updated_on ? metadata.updated_on : '1970-01-01',
-		statistic,
-		metadata,
 		history: isFailed ? 'F' : 'P',
+		...metadata,
+		results,
 	};
 
+	const reports = json[event_name][group].reports;
 	console.log( report );
-	const reportIndex = json.reports.findIndex( r => r.id === reportId );
+	const reportIndex = reports.findIndex( r => r.report_id === report_id );
 
 	if ( reportIndex !== -1 ) {
 		// Update the report entry in the reports list
-		if ( json.reports[ reportIndex ].history ) {
-			report.history = json.reports[ reportIndex ].history + report.history;
+		if ( reports[ reportIndex ].history ) {
+			report.history = reports[ reportIndex ].history + report.history;
 			report.history = report.history.substring(report.history.length - 200);
 		}
-		json.reports[ reportIndex ] = report;
+		reports[ reportIndex ] = report;
 	} else {
 		// push new report
-		json.reports.push( report );
+		reports.push( report );
 	}
 
-	json.reportsCount = json.reports.length;
 	json.lastUpdate = new Date().toISOString();
+	return json;
 }
