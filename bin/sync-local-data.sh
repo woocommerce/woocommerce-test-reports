120a9f2c2d28fb2a331f6d051de522387b53e10f
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-06-17 16:22:18 +0300

    Add script to sync local data

---
+++ bin/sync-local-data.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")" || return
+  pwd -P
+)
+
+aws s3 sync s3://a8c-woo-test-reports/data "$SCRIPT_PATH/../public/data" --delete
