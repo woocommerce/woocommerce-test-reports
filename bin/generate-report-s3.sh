bbb5e725beea7ff6fe268ec0e64bbd339752a7bb
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-06-17 09:55:47 +0300

    Generate report with run_number and run_attempt

--- bin/generate-report-s3.sh
+++ bin/generate-report-s3.sh
@@ -85,6 +85,8 @@ METADATA="{
   \"group\": \"$REPORT_GROUP\",
   \"ref_name\": \"$REF_NAME\",
   \"run_id\": \"$RUN_ID\",
+  \"run_number\": \"$RUN_NUMBER\",
+  \"run_attempt\": \"$RUN_ATTEMPT\",
   \"event_name\": \"$EVENT_NAME\",
   \"report_title\": \"$REPORT_TITLE\",
   \"pr_number\": \"$PR_NUMBER\",
