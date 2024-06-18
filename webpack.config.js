f63b80a2471b0eebf55796646297fe82675600b4
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-06-18 23:46:47 +0300

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require( 'path' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+const manifest = require('./manifest.json');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve( __dirname, 'dist' ),
+		filename: 'bundle.js',
+		publicPath: '/woocommerce-test-reports/',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ],
+			},
+			{
+				test: /\.(png|svg)$/,
+				use: [ 'file-loader' ],
+			},
+			{
+				test: /\.json$/,
+				type: 'javascript/auto',
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					{
+						loader: 'json-loader',
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+	  new HtmlWebpackPlugin({
+		template: './src/templates/index.html',
+		favicon: './src/assets/favicon.ico',
+		title: manifest.name,
+		meta: {
+        	description: manifest.description,
+      },
+	  }),
+	  new CopyWebpackPlugin({
+		patterns: [
+			{ from: 'data', to: 'data' },
+			{ from: 'src/static', to: '.' },
+			{ from: 'manifest.json', to: 'manifest.json' },
+		],
+	  }),
+	],
+	devServer: {
+		static: {
+			directory: path.join( __dirname, 'dist' ),
+		},
+		compress: true,
+		port: 3000,
+	},
+};
