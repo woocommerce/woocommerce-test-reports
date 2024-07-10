8b0bcc900a9d2be7d0e15ca662d1e6cfe140fb22
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-07-10 17:39:09 +0300

    Include delta field in failure-rates data file

--- bin/update-stats.js
+++ bin/update-stats.js
@@ -91,7 +91,7 @@ const entryTemplate =
 	sort( weeklyJson, 'date', true );
 
     let failureRates = weeklyJson.map(week => ( {
-        date: week.date, trunk: week.trunk.testsFailedRate, total: week.total.testsFailedRate
+        date: week.date, trunk: week.trunk.testsFailedRate, total: week.total.testsFailedRate, delta: week.total.testsFailedRate - week.trunk.testsFailedRate
     }));
 
 
