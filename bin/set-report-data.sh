f1e584801dccbfb4468cf745e6d3084a11a41759
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-07-24 22:11:51 +0300

    Update report metadata for on demand runs

--- bin/set-report-data.sh
+++ bin/set-report-data.sh
@@ -67,9 +67,8 @@ elif [[ "$EVENT_NAME" == "pull_request" ]] || [[ "$EVENT_NAME" == "pr" ]]; then
     REPORT_GROUP=$PR_NUMBER
 elif [[ "$EVENT_NAME" == "on-demand" ]] ; then
     EVENT_NAME="other"
-    REPORT_TITLE="On demand $(date +%Y-%m-%d) $REF_NAME-$COMMIT_SHA"
-    REPORT_GROUP="on-demand-$REF_NAME-$COMMIT_SHA"
-    REPORT_GROUP=$(echo "$REPORT_GROUP" | tr ' /. ' '_')
+    REPORT_TITLE="On demand $REF_NAME-$COMMIT_SHA"
+    REPORT_GROUP=$(echo "$REPORT_TITLE" | tr ' /. ' '_')
 else
   echo "Unknown event name: $EVENT_NAME"
   exit 1
