38fe00a934d632615a4f6579c0668dab49b0de30
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-08-23 18:17:10 +0300

    Update local data path in sync script

--- bin/sync-local-data.sh
+++ bin/sync-local-data.sh
@@ -5,4 +5,5 @@ SCRIPT_PATH=$(
   pwd -P
 )
 
+aws s3 sync s3://a8c-woo-test-reports/data "$SCRIPT_PATH/../data" --delete
 aws s3 sync s3://a8c-woo-test-reports/data "$SCRIPT_PATH/../dist/data" --delete
