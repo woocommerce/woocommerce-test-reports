6401e0b47e1e9cb19057eb61bf5807fdd83cb904
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-09-04 12:17:45 +0300

    Remove unused script

--- bin/deploy-dev-to-s3.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_PATH=$(
-  cd "$(dirname "${BASH_SOURCE[0]}")" || return
-  pwd -P
-)
-
-aws s3 sync "$SCRIPT_PATH/../dist" s3://a8c-woo-test-reports/woocommerce-test-reports --delete
